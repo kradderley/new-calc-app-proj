310379f9c4ca8ab00c4e9ff2a3c58827b8e096e5
Khes Adderley <khesadderley@Khess-MacBook-Air.local>
2023-12-19 15:06:27 -0500

    next commit

--- calculator-proj/src/components/calculator.jsx
+++ calculator-proj/src/components/calculator.jsx
@@ -16,26 +16,25 @@ const Calculator = () => {
             <input type="text" placeholder="0" className="input-field" />
           </div>
           <div className="keypad">
-            <button className="keypad-number">AC</button>
-            <button className="keypad-number">/</button>
-            <button className="keypad-number">%</button>
-            <button className="keypad-number">÷</button>
+            <button className="keypad-number signs">AC</button>
+            <button className="keypad-number signs">C</button>
+            <button className="keypad-number signs">%</button>
+            <button className="keypad-number signs">÷</button>
             <button className="keypad-number">7</button>
             <button className="keypad-number">8</button>
             <button className="keypad-number">9</button>
-            <button className="keypad-number">x</button>
+            <button className="keypad-number signs">x</button>
             <button className="keypad-number">4</button>
             <button className="keypad-number">5</button>
             <button className="keypad-number">6</button>
-            <button className="keypad-number">-</button>
+            <button className="keypad-number signs">-</button>
             <button className="keypad-number">1</button>
             <button className="keypad-number">2</button>
             <button className="keypad-number">3</button>
-            <button className="keypad-number">+</button>
-            <button className="keypad-number">0</button>
-            <button className="keypad-number">2</button>
-            <button className="keypad-number">,</button>
-            <button className="keypad-number">=</button>
+            <button className="keypad-number signs">+</button>
+            <button className="keypad-number zero">0</button>
+            <button className="keypad-number signs zero">,</button>
+            <button className="keypad-number signs zero">=</button>
           </div>
         </div>
       </section>
