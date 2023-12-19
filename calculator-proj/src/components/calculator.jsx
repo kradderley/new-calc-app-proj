5ca48a1ff39edd38cdc331178751eabef08af7e5
Khes Adderley <khesadderley@Khess-MacBook-Air.local>
2023-12-19 11:46:01 -0500

    background-fixed

--- calculator-proj/src/components/calculator.jsx
+++ calculator-proj/src/components/calculator.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import "./calculator.css";
 import styled from "styled-components";
 
 const Calculator = () => {
   return (
     <>
-      <div>Hello</div>
+      <StyledSection>
+        <section>
+          <div className="sectionOne">Hello</div>
+        </section>
+      </StyledSection>
     </>
   );
 };
+
+const StyledSection = styled.section`
+  background-color: gray;
+  height: 100vh;
+  width: 100vw;
+  color: white;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  max-width: 390px;
+  max-height: 800px;
+  overflow: hidden;
+  text-align: center;
+`;
 
 export default Calculator;
